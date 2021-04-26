9e7087cd4e8d76f3320cf18ba2ca8962ba71e2e1
Vuqar <vuqarss@hotmail.com>
2021-04-26 13:27:13 +0400

    Added PPF

---
+++ Filters/PerPixelFormat/InvertFilter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using processimage.BitmapHelper;
+
+namespace processimage.Filters
+{
+    /// <summary>
+    /// Invet filter
+    /// </summary>
+    class InvertFilter
+    {
+        string outFileName;
+        string filePath;
+
+
+        /// <summary>
+        /// Takes filePath of image that filter applies on. And takes outFileName(path) where result image will be saved.
+        /// </summary>
+        /// <param name="_filePath"></param>
+        /// <param name="_outFileName"></param>
+        public InvertFilter(string _filePath, string _outFileName)
+        {
+            outFileName = _outFileName;
+            filePath = _filePath;
+        }
+
+        public void Apply()
+        {
+            (int Stride, byte[] imageBytes, int Width, int Height) = new BitmapDataExtractor(filePath);
+            byte[] OutputBytes = imageBytes;
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int a = 0; a < Stride; a = a + 1)
+                {
+                    imageBytes[i * Stride + a] = (byte)(255 - imageBytes[i * Stride + a]);
+                }
+            }
+            BitmapBsSaver.Save(imageBytes, outFileName, Width, Height); // saves bitmap with specified byte array
+        }
+    }
+}
---
+++ Filters/PerPixelFormat/PerPixelFilterApplyer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace processimage.Filters
+{
+    /// <summary>
+    /// Gets filter then switchs it then calls proper Filter
+    /// </summary>
+    class PerPixelFilterApplyer
+    {
+        FilterOptions filter;
+        string filePath;
+        
+
+        public PerPixelFilterApplyer(FilterOptions _filter, string _filePath)
+        {
+            
+            filter = _filter;
+            filePath = _filePath;
+           
+        }
+
+        public void ApplyFilter(string outputFileName, int level=50) {
+            switch (filter) {
+                case FilterOptions.Grayscale:
+                    Log.Debug("FilePath: {path}",filePath);
+                    new GrayscaleFilter(filePath,outputFileName).Apply();
+                    break;
+                case FilterOptions.Invert:
+                    Log.Debug("FilePath: {path}", filePath);
+                    new InvertFilter(filePath, outputFileName).Apply();
+                    break;
+                case FilterOptions.Brightness:
+                    Log.Debug("FilePath: {path}", filePath);
+                    new BrightnessFilter(filePath, outputFileName,level).Apply();
+                    break;
+            }
+        }
+
+    }
+}
